18daf5414fe0e38e615fd5c296e0a1ce47b1fcf8
ghazapratama <fghaza.pratama@gmail.com>
2021-05-14 19:09:42 +0700

    menghilangkan javascript

--- product/script.js
+++ product/script.js
@@ -1,4 +1,4 @@
-for(var i = 0; i < 1; i++) {
-    alert('Oops halaman kosong :(');
-    alert('Untuk sementara halaman ini kosong dikarenakan tidak tau mau di isi apa');
-}
+// for(var i = 0; i < 1; i++) {
+//     alert('Oops halaman kosong :(');
+//     alert('Untuk sementara halaman ini kosong dikarenakan tidak tau mau di isi apa');
+// }
