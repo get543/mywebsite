386ab30c322767cce06e52300ff723862884382d
Udin <fghaza.pratama@gmail.com>
2022-07-18 00:07:53 +0700

    fixing broken things

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-for(var i = 0; i < 1; i++) {
-    alert('Oops halaman kosong :(');
-    alert('Untuk sementara halaman ini kosong dikarenakan tidak tau mau di isi apa');
+for (var i = 0; i < 1; i++) {
+  alert("Oops halaman kosong :(");
+  alert(
+    "Untuk sementara halaman ini kosong dikarenakan tidak tau mau di isi apa"
+  );
 }
